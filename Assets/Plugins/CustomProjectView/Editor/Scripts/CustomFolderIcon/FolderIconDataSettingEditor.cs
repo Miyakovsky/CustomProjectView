954059e0aefed1add39897fb2baa2b6536b6b66a
Miyakovsky <yamaneko116@yahoo.co.jp>
2018-07-28 17:20:14 +0900

    fix remove
    不要なのを削除と対応

--- Assets/Plugins/CustomProjectView/Editor/Scripts/CustomFolderIcon/FolderIconDataSettingEditor.cs
+++ Assets/Plugins/CustomProjectView/Editor/Scripts/CustomFolderIcon/FolderIconDataSettingEditor.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using Borodar.ReorderableList;
+using UnityEditorInternal;
+//using Borodar.ReorderableList;
 
 namespace CustomProjectView.CustomFolderIcon
 {
@@ -10,19 +11,34 @@ namespace CustomProjectView.CustomFolderIcon
     public class FolderIconDataSettingEditor : Editor
     {
         private const string PROP_NAME_FOLDERS = "Folders";
+        private const int HeightSize = 90;
 
-        private SerializedProperty _foldersProperty;
+        ReorderableList _reorderableList;
 
         protected void OnEnable()
         {
-            _foldersProperty = serializedObject.FindProperty(PROP_NAME_FOLDERS);
+            //リストの表示設定
+            var prop = serializedObject.FindProperty(PROP_NAME_FOLDERS);
+            _reorderableList = new ReorderableList(serializedObject, prop);
+
+            _reorderableList.elementHeight = HeightSize;
+            _reorderableList.drawElementCallback =
+              (rect, index, isActive, isFocused) => {
+                  var element = prop.GetArrayElementAtIndex(index);
+                  EditorGUI.PropertyField(rect, element);
+              };
+
+            var defaultColor = GUI.backgroundColor;
+
+            _reorderableList.drawHeaderCallback = (rect) =>
+              EditorGUI.LabelField(rect, prop.displayName);
         }
 
         public override void OnInspectorGUI()
         {
+            //表示更新
             serializedObject.Update();
-            ReorderableListGUI.Title("Custom Folder Icon");
-            ReorderableListGUI.ListField(_foldersProperty);
+            _reorderableList.DoLayoutList();
             serializedObject.ApplyModifiedProperties();
         }
     }
