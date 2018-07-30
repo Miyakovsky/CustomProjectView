ed212637ec5d8a07973c2bef0a9c227602b1a7b9
Miyakovsky <yamaneko116@yahoo.co.jp>
2018-07-31 02:25:05 +0900

    Fix
    更新

--- Assets/Plugins/CustomProjectView/Editor/Scripts/CustomProjectView.cs
+++ Assets/Plugins/CustomProjectView/Editor/Scripts/CustomProjectView.cs
@@ -5,8 +5,40 @@ using UnityEngine;
 namespace CustomProjectView
 {
     [InitializeOnLoad]
-    public class CustomProjectView
+    public class CustomProjectView : EditorWindow
     {
+        private static bool _isCustomFolderIconFlag = true;
+        private static bool _isCustomFolderIconSetting = true;
+
+        [MenuItem("Tools/CustomProjectView")]
+        static void Open()
+        {
+            _isCustomFolderIconSetting = _isCustomFolderIconFlag;
+
+            GetWindow<CustomProjectView>();
+        }
+
+        void OnGUI()
+        {
+
+            EditorGUILayout.LabelField("カスタムプロジェクトビュー設定");
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            {
+                _isCustomFolderIconSetting = EditorGUILayout.Toggle("フォルダアイコンの適応", _isCustomFolderIconSetting);
+            }
+            EditorGUILayout.EndVertical();
+
+            if (GUILayout.Button("設定"))
+            {
+                _isCustomFolderIconFlag = _isCustomFolderIconSetting;
+                Close();
+            }
+        }
+
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
         static CustomProjectView()
         {
             EditorApplication.projectWindowItemOnGUI += OnGUI;
@@ -15,12 +47,18 @@ namespace CustomProjectView
 
         }
 
+
+        /// <summary>
+        /// プロジェクトビューの表示追加更新
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <param name="selectionRect"></param>
         private static void OnGUI(string guid, Rect selectionRect)
         {
-            CustomFolderIcon.CustomFolderIcon.OnGUI(guid, selectionRect);
-
-
-
+            if(_isCustomFolderIconFlag)
+            {
+                CustomFolderIcon.CustomFolderIcon.OnGUI(guid, selectionRect);
+            }
         }
     }
 }
